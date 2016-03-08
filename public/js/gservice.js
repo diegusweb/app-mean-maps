2aa2f6a5c0846e2e937ad79427388c282c20cc0c
Diego Rueda <diego.rueda2@gmail.com>
2016-03-08 17:57:47 -0400

    Adding Clickability

--- public/js/gservice.js
+++ public/js/gservice.js
@@ -1,6 +1,6 @@
 // Creates the gservice factory. This will be the primary means by which we interact with Google Maps
 angular.module('gservice', [])
-    .factory('gservice', function($http){
+    .factory('gservice', function($rootScope, $http){
 
         // Initialize Variables
         // -------------------------------------------------------------
@@ -109,20 +109,39 @@ var initialize = function(latitude, longitude) {
     });
 
     // Set initial location as a bouncing red marker
-    var initialLocation = new google.maps.LatLng(latitude, longitude);
-    var marker = new google.maps.Marker({
-        position: initialLocation,
-        animation: google.maps.Animation.BOUNCE,
-        map: map,
-        icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+    // Function for moving to a selected location
+    map.panTo(new google.maps.LatLng(latitude, longitude));
+
+    // Clicking on the Map moves the bouncing red marker
+    google.maps.event.addListener(map, 'click', function(e){
+        var marker = new google.maps.Marker({
+            position: e.latLng,
+            animation: google.maps.Animation.BOUNCE,
+            map: map,
+            icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+        });
+
+        googleMapService.clickLat = marker.getPosition().lat();
+googleMapService.clickLong = marker.getPosition().lng();
+$rootScope.$broadcast("clicked");
+
+        // When a new spot is selected, delete the old red bouncing marker
+        if(lastMarker){
+            lastMarker.setMap(null);
+        }
+
+        // Create a new red bouncing marker and move to it
+        lastMarker = marker;
+        map.panTo(marker.position);
     });
-    lastMarker = marker;
 
 };
 
 // Refresh the page upon window load. Use the initial latitude and longitude
 google.maps.event.addDomListener(window, 'load',
     googleMapService.refresh(selectedLat, selectedLong));
+
+
 
 return googleMapService;
 });
